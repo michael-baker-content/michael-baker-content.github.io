02fc0ba2a10585eb5824f408cacda59de7d254e0
Michael Baker <polyphonic@hotmail.com>
2026-04-17 14:22:32 -0700

    Mobile nav, site title update, about page, sticky footer fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'michael-baker-content';
+export const SITE_TITLE = 'Michael Baker';
 export const SITE_DESCRIPTION = 'In-depth writing about code, creative tools, and projects in progress.';
