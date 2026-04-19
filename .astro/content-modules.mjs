4817092124c58b33f65344230357fbac871f62f4
Michael Baker <polyphonic@hotmail.com>
2026-04-19 14:03:39 -0700

    Add Strudel live coding embed component and demo post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
 ["src/content/blog/p5js-generative-art.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fp5js-generative-art.mdx&astroContentModuleFlag=true")],
-["src/content/blog/python-data-science-intro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpython-data-science-intro.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/python-data-science-intro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpython-data-science-intro.mdx&astroContentModuleFlag=true")],
+["src/content/blog/strudel-live-coding.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstrudel-live-coding.mdx&astroContentModuleFlag=true")]]);
 		
